6cc6a525731a8a9c5a7e4abf2bba45872d071c2b
it24103039 <it24103039@my.sliit.lk>
2024-09-23 23:23:28 +0530

    Add IT24103039Lab7Q2 to Q2A

---
+++ Q2/IT24103039Lab7Q2A.java
@@ -0,0 +1,19 @@
+public class IT24103039Lab7Q2A	{
+	public static void main(String[] args)	{
+	
+	//Defining variables
+	int rowCounter,columnCounter;
+
+	//Entering the outside for loop 
+	for(rowCounter=1;rowCounter<=4;rowCounter++)
+	{
+	    for(columnCounter=1;columnCounter<=5;columnCounter++)
+		{
+		   System.out.print(" $ ");
+		}
+		System.out.println();
+		
+	}
+	
+     }
+}
