5b5fd5a5fb70c9223d0d880b7a1944c951dcdde6
it24103039 <it24103039@my.sliit.lk>
2024-09-23 23:26:26 +0530

    Add IT24103039Lab7Q2 to Q2C

---
+++ Q2/IT24103039Lab7Q2C.java
@@ -0,0 +1,18 @@
+public class IT24103039Lab7Q2C   {
+    public static void main(String[] args)	{
+
+	//Defining variables 
+	int rowCounter,columnCounter;
+	
+	//Creating the nested loop with two for loops
+	for(rowCounter=5;rowCounter>=1;rowCounter--)
+	{
+	    for(columnCounter=rowCounter;columnCounter>=1;columnCounter--)
+	      {
+		System.out.print(rowCounter );
+		}
+		
+		System.out.println();
+	}
+    }
+}
